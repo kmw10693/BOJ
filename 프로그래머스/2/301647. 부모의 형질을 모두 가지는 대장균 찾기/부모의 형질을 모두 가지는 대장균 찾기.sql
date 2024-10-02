a9348159cf8b8fe0a32e0064148457aa6c197939
Minu Kim <kmw106933@naver.com>
2024-10-02 20:30:31 +0900

    [level 2] Title: 부모의 형질을 모두 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/301647. 부모의 형질을 모두 가지는 대장균 찾기/부모의 형질을 모두 가지는 대장균 찾기.sql
+++ 프로그래머스/2/301647. 부모의 형질을 모두 가지는 대장균 찾기/부모의 형질을 모두 가지는 대장균 찾기.sql
@@ -1,6 +1,5 @@
 -- 코드를 작성해주세요
 select A.ID, A.GENOTYPE, B.GENOTYPE as PARENT_GENOTYPE
 from ECOLI_DATA A, ECOLI_DATA B
-where A.PARENT_ID = B.ID
-and A.GENOTYPE & B.GENOTYPE = B.GENOTYPE
-order by A.id asc
+where A.PARENT_ID = B.ID and A.GENOTYPE & B.GENOTYPE = B.GENOTYPE
+order by ID asc;
