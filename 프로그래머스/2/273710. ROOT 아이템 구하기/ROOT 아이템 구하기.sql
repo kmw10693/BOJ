873011756642d76af1fd5cdcde06f45205a2cccf
Minu Kim <kmw106933@naver.com>
2024-10-08 23:10:10 +0900

    [level 2] Title: ROOT 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
+++ 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
@@ -1,4 +1,5 @@
 -- 코드를 작성해주세요
-select a.ITEM_ID, a.ITEM_NAME from ITEM_INFO as a
-inner join ITEM_TREE b on a.ITEM_ID = b.ITEM_ID
-where ISNULL(PARENT_ITEM_ID) = 1
+SELECT T.ITEM_ID, ITEM_NAME from ITEM_TREE T
+join ITEM_INFO I on T.ITEM_ID = I.ITEM_ID
+where PARENT_ITEM_ID is NULL
+order by T.ITEM_ID asc;
