dad0d076c4c5afacb855b2b002796be52da19ce6
Minu Kim <kmw106933@naver.com>
2024-11-12 15:04:01 +0900

    [Silver I] Title: 정수 삼각형, Time: 128 ms, Memory: 35612 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,15 @@
+n = int(input())
+data = []
+for i in range(n):
+    data.append(list(map(int, input().split())))
+    
+for i in range(1, n):
+    for j in range(i+1):
+        if j == 0: data[i][j] = data[i-1][j] + data[i][j]
+        
+        elif i == j: data[i][j] = data[i-1][j-1] + data[i][j]
+        
+        else:
+            data[i][j] = max(data[i-1][j-1], data[i-1][j]) + data[i][j]
+
+print(max(data[n-1]))
