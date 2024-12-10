00fd97e512d39ef8675264a4124668bab5a99666
Minu Kim <kmw106933@naver.com>
2024-12-10 11:57:45 +0900

    [Silver IV] Title: ATM, Time: 92 ms, Memory: 109544 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+arr.sort()
+
+ans = 0
+for i in range(n):
+    for j in range(0, i+1):
+        ans += arr[j]
+print(ans)
