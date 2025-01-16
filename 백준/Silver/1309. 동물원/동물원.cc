6ade27e0452bb0015f013542a09270014ee07616
Minu Kim <kmw106933@naver.com>
2025-01-16 16:18:28 +0900

    [Silver I] Title: 동물원, Time: 0 ms, Memory: 3192 KB -BaekjoonHub

---
+++ 백준/Silver/1309. 동물원/동물원.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N;
+int dp[100005][3];
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    
+    cin >> N;
+    
+    dp[1][0] = 1;
+    dp[1][1] = 1;
+    dp[1][2] = 1;
+    
+    for(int i=2; i<=N; i++) {
+        dp[i][0] = (dp[i-1][0]+ dp[i-1][1] + dp[i-1][2]) % 9901;
+        dp[i][1] = (dp[i-1][0] + dp[i-1][2]) % 9901;
+        dp[i][2] = (dp[i-1][0] +dp[i-1][1]) % 9901;
+    }
+    
+    cout << (dp[N][0] + dp[N][1] + dp[N][2]) % 9901;
+} 
