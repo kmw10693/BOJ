dc798f0d1deea2f67a3403172b4c4425ec6ec0d6
Minu Kim <kmw106933@naver.com>
2025-01-21 11:11:54 +0900

    [level 2] Title: 전화번호 목록, Time: 717.04 ms, Memory: 59.1 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -11,8 +11,8 @@ bool solution(vector<string> phone_book) {
     }
     
     for(int i=0; i<phone_book.size(); i++) {
-        for(int j=0; j<phone_book[i].length(); j++) {
-            string sub = phone_book[i].substr(0, j);
+        for(int j=0; j<phone_book[i].length()-1; j++) {
+            string sub = phone_book[i].substr(0, j+1);
             if(mm[sub]) return false;
         }
     }
