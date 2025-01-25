490c470480c26e4d707bf32af1eee588e3b115c1
Minu Kim <kmw106933@naver.com>
2025-01-26 01:36:35 +0900

    [level 1] Title: 특정 형질을 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
+++ 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
@@ -1,4 +1,4 @@
 -- 코드를 작성해주세요
-select count(ID) as COUNT
-from ECOLI_DATA
-where CONV(GENOTYPE, 10, 2) not like '%1_' and (CONV(GENOTYPE, 10, 2) like '%1' or CONV(GENOTYPE, 10, 2) like '%1__')
+select count(*) as COUNT from ECOLI_DATA
+where CONV(GENOTYPE, 10, 2) not like '%1_' and
+(CONV(GENOTYPE, 10, 2) like '%1' or CONV(GENOTYPE, 10, 2) like '%1__');
