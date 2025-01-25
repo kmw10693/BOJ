c5f5f4c676c1b1a512589c227e195bf712d2920f
Minu Kim <kmw106933@naver.com>
2025-01-26 01:06:17 +0900

    [level 1] Title: Python 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
+++ 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
@@ -1,4 +1,4 @@
 -- 코드를 작성해주세요
-select ID, EMAIL, FIRST_NAME, LAST_NAME from DEVELOPER_INFOS
-where 'Python' in (SKILL_1, SKILL_2, SKILL_3)
-order by id
+SELECT ID, EMAIL, FIRST_NAME, LAST_NAME from DEVELOPER_INFOS
+where SKILL_1 = "PYTHON" or SKILL_2 = "PYTHON" or SKILL_3 = "PYTHON"
+order by ID;
