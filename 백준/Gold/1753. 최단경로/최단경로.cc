2f7eb041ca4a84379033692fc7889a8de38fc3d6
Minu Kim <kmw106933@naver.com>
2025-01-30 02:00:28 +0900

    [Gold IV] Title: 최단경로, Time: 88 ms, Memory: 9184 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define X first
+#define Y second
+
+int v,e,st;
+// w, v
+vector<pair<int,int>> adj[20005];
+const int INF = 1e9 + 10;
+int d[20005];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> v >> e >> st;
+    
+    for(int i=0; i<e; i++) {
+        int u,v,w;
+        cin >> u >> v >> w;
+        adj[u].push_back({w, v});
+    }
+    
+    fill(d, d+v+1, INF);
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+    d[st] = 0;
+    pq.push({d[st], st});
+    
+    while(!pq.empty()) {
+        auto cur = pq.top(); pq.pop();
+        if(d[cur.Y] != cur.X) continue;
+        for(auto nxt : adj[cur.Y]) {
+            if(d[nxt.Y] <= d[cur.Y] + nxt.X) continue;
+            d[nxt.Y] = d[cur.Y] + nxt.X;
+            pq.push({d[nxt.Y], nxt.Y});
+        }
+    }
+    for(int i=1; i<=v; i++) {
+        if(d[i] == INF) cout << "INF";
+        else cout << d[i];
+        cout << '\n';
+    }
+}
