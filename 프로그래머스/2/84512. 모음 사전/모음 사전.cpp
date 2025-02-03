9193c80c8d34382d13794249bfa217e8bf606c6a
Minu Kim <kmw106933@naver.com>
2025-02-03 16:27:34 +0900

    [level 2] Title: 모음 사전, Time: 0.14 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
@@ -1,36 +1,33 @@
 #include <string>
 #include <vector>
-#include <bits/stdc++.h>
 
 using namespace std;
-string answer;
-int answer2;
-int k = 0;
 
-void backtrack(string word, int cnt) {
-    if(word == answer) { 
-        answer2 = k;
+int k = 0;
+string answerWord;
+int answer = 0;
+
+void backtrack(string s, int cnt) {
+    if(s == answerWord) {
+        answer = k;
         return;
     }
-    
-    if(cnt == 5) {
-        return;
-    }
+    if(cnt >= 5) return;
     
     k++;
-    backtrack(word+"A", cnt+1);
+    backtrack(s+"A", cnt+1);
     k++;
-    backtrack(word+"E", cnt+1);
+    backtrack(s+"E", cnt+1);
     k++;
-    backtrack(word+"I", cnt+1);
+    backtrack(s+"I", cnt+1);
     k++;
-    backtrack(word+"O", cnt+1);
+    backtrack(s+"O", cnt+1);
     k++;
-    backtrack(word+"U", cnt+1);
+    backtrack(s+"U", cnt+1);
 }
 
 int solution(string word) {
-    answer = word;
+    answerWord = word;
     backtrack("", 0);
-    return answer2;
+    return answer;
 }
