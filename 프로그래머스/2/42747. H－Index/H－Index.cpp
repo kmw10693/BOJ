50e92e33ca233a02c24f65b166beb6027714d0e4
Minu Kim <kmw106933@naver.com>
2025-02-06 01:41:02 +0900

    [level 2] Title: H-Index, Time: 0.96 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.cpp
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -3,19 +3,20 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-int answer = -1;
 
 int solution(vector<int> citations) {
     sort(citations.begin(), citations.end());
     int minV = citations.at(0);
     int maxV = citations.at(citations.size() - 1);
     
-    for(int i=0; i<=maxV; i++) {
-        int tmp = 0;
-        for(int j=0; j<citations.size(); j++) {
-            if(i <= citations[j]) tmp++;
-        }        
-        if(tmp >= i) answer = max(i, answer);
+    int answer = 0;
+    for(int j=0; j<=maxV; j++) {
+        int cnt = 0;
+        int cnt2 = 0;
+        for(int i=0; i<citations.size(); i++) {
+            if(citations[i] >= j) cnt++;
+        }
+        if(cnt >= j) answer = j;
     }
     return answer;
 }
