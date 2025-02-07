79ddf43a979e709585c7c0481f0fb5b5548e27c3
Minu Kim <kmw106933@naver.com>
2025-02-08 08:18:54 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,2 +1,6 @@
 -- 코드를 입력하세요
-SELECT
+SET @hour = -1;
+select (@hour := @hour + 1) as HOUR,
+(select count(*) from ANIMAL_OUTS where HOUR(DATETIME) = @HOUR) as COUNT
+from ANIMAL_OUTS
+where @hour < 23;
