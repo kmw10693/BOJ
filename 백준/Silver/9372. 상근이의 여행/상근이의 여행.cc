ffe40e7f92f2d6b6aaf55e65f6e9ff6b97558855
Minu Kim <kmw106933@naver.com>
2025-03-20 10:48:26 +0900

    [Silver IV] Title: 상근이의 여행, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int t;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> t;
+    
+    while(t--) {
+        int n, m;
+        cin >> n >> m;
+        
+        while(m--) {
+            int a, b;
+            cin >> a >> b;
+        }
+        cout << (n-1) << '\n';
+    }
+
+}
