d704424467dfa004e0d653435f528e26f2376890
Minu Kim <kmw106933@naver.com>
2025-04-19 16:53:30 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
@@ -1,87 +1,56 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int n;
-
-int arr[13];
-
-vector <int> v;
-
-int ans2(int a, int b, int t);
-
-int ans(){
-
-    int sum = 0;
-
-    for(int i=0; i<v.size(); i++){
-
-        if(i == 0) sum = ans2(arr[i], arr[i+1], v[i]);
-
-        else sum = ans2(sum, arr[i+1], v[i]);
-
-    }
-
-    return sum;
-
-}
-
-int ans2(int a, int b, int t){
-
-    if(t == 0) return a+b;
-
-    else if(t == 1) return a-b;
-
-    else if(t == 2) return a*b;
-
-    return a/b;
-
-}
+int maxV = -0x7f7f7f7f;
+int minV = 0x7f7f7f7f;
 
 int main() {
-
     ios::sync_with_stdio(0);
-
     cin.tie(0);
-
     
-
+    int n;
     cin >> n;
-
-    for(int i=0; i<n; i++)
-
-      cin >> arr[i];
-
-      
-
-      
-
-    for(int i=0; i<4; i++){
-
-        int t;
-
-        cin >> t;
-
-        for(int j=0; j<t; j++){
-
-            v.push_back(i);
-
-        }
-
+    
+    vector<int> v;
+    for(int i=0; i<n; i++) {
+        int a; cin >> a;
+        v.push_back(a);
     }
-
-    sort(v.begin(), v.end());
-
-    int maxt =-1999999999, mint = 1999999999;
-
-    do{
-
-        maxt = max(maxt, ans());
-
-        mint = min(mint, ans());
-
-    }while(next_permutation(v.begin(), v.end()));
-
-    cout << maxt << '\n' << mint;
-
+    
+    // 연산자 덧셈 1, 뺄셈 2, 나눗셈 3, 곱셈 4
+    vector<int> op;
+    for(int i=0; i<4; i++) {
+        int v; cin >> v;
+        for(int j=0; j<v; j++) {
+            op.push_back(i+1);
+        }
+    }
+    
+    // 이제 랜덤으로 섞기 전 정렬
+    sort(op.begin(), op.end());
+    
+    do {
+        // 섞고 v 처음부터 순차적으로 계산
+        int first = v[0];
+        // 두번째부터 시작 0 ~ n-2 성립
+        for(int i=1; i<n; i++) {
+            if(op[i-1] == 1) {
+                first += v[i];
+            }
+            else if(op[i-1] == 2) {
+                first -= v[i];
+            }
+            else if(op[i-1] == 3) {
+                first *= v[i];
+            }
+            else {
+                first /= v[i];
+            }
+        }
+        // 최댓값 정립
+        maxV = max(maxV, first);
+        minV = min(minV, first);
+    } while(next_permutation(op.begin(), op.end()));
+    
+    cout << maxV << '\n' << minV;
 }
