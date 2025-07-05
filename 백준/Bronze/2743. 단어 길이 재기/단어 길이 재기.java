cf21378400d4b8c34a02743d0f5940e318dd1a6f
Minu Kim <kmw106933@naver.com>
2025-07-06 00:00:00 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 168 ms, Memory: 17608 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String st = sc.next();
+        sc.close();
+        
+        System.out.println(st.length());
+    }
+}
