c607c476362d7121e3151d3b31e70a9b976d5545
Minu Kim <kmw106933@naver.com>
2025-08-29 21:10:08 +0900

    [Platinum IV] Title: 탈옥, Time: 304 ms, Memory: 28972 KB -BaekjoonHub

--- 백준/Platinum/9376. 탈옥/탈옥.java
+++ 백준/Platinum/9376. 탈옥/탈옥.java
@@ -1,105 +1,108 @@
 import java.io.*;
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static class Position implements Comparable<Position> {
-        int x, y, count;
+    private static class Person implements Comparable<Person> {
+        int x, y, score;
 
-        public Position(int x, int y, int count) {
+        Person(int x, int y, int score) {
             this.x = x;
             this.y = y;
-            this.count = count;
+            this.score = score;
         }
 
         @Override
-        public int compareTo(Position o) {
-            return this.count - o.count;
+        public int compareTo(Person o) {
+            return this.score - o.score;
         }
+
     }
 
-    static char[][] map;
-    static ArrayList<Position> prisoner;
-    static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {-1, 1, 0, 0};
-    static boolean[][] visited;
-    static int[][] prisionOne, prisionTwo, prisonThree;
+    private static int N;
+    private static int h, w;
+    private static char[][] map;
+    private static ArrayList<Person> prisoner;
+    private static boolean[][] visited;
+    private static int[] dx = {1, -1, 0, 0};
+    private static int[] dy = {0, 0, 1, -1};
+    private static int ans;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        N = Integer.parseInt(br.readLine());
 
-        StringTokenizer st;
-        int T = Integer.parseInt(br.readLine());
-        for (int i = 0; i < T; i++) {
-            st = new StringTokenizer(br.readLine());
-            int h = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
+        while (N > 0) {
+            ans = 0x7f7f7f7f;
             prisoner = new ArrayList<>();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            h = Integer.parseInt(st.nextToken());
+            w = Integer.parseInt(st.nextToken());
             map = new char[h + 2][w + 2];
-            prisoner.add(new Position(0, 0, 0));
-            for (int j = 0; j < h; j++) {
-                String str = br.readLine();
-                for (int s = 0; s < w; s++) {
-                    map[j + 1][s + 1] = str.charAt(s);
-                    if (map[j + 1][s + 1] == '$') {
-                        prisoner.add(new Position(s + 1, j + 1, 0));
+            for (int i = 1; i < h + 1; i++) {
+                String s = br.readLine();
+                for (int j = 0; j < w; j++) {
+                    map[i][j+1] = s.charAt(j);
+                    if (map[i][j+1] == '$') {
+                        prisoner.add(new Person(i, j+1, 0));
                     }
                 }
             }
-            prisionOne = bfs(prisoner.get(0), h, w);
-            prisionTwo = bfs(prisoner.get(1), h, w);
-            prisonThree = bfs(prisoner.get(2), h, w);
-            bw.write(minDoor(h, w) + "\n");
+
+            int[][] one = bfs(prisoner.get(0));
+            int[][] two = bfs(prisoner.get(1));
+            int[][] three = bfs(new Person(0,0, 0));
+            calculate(one, two, three);
+            System.out.println(ans);
+            N--;
         }
-        bw.flush();
-        bw.close();
-        br.close();
     }
 
-    static int[][] bfs(Position p, int h, int w) {
-        PriorityQueue<Position> pq = new PriorityQueue<>();
+    private static int[][] bfs(Person p) {
+        PriorityQueue<Person> pq = new PriorityQueue<>();
         visited = new boolean[h + 2][w + 2];
         int[][] result = new int[h + 2][w + 2];
-        visited[p.y][p.x] = true;
         pq.add(p);
+        visited[p.x][p.y] = true;
+        result[p.x][p.y] = p.score;
+
         while (!pq.isEmpty()) {
-            Position cur = pq.poll();
-            int x = cur.x;
-            int y = cur.y;
-            int count = cur.count;
-            result[y][x] = count;
+            Person ps = pq.poll();
+            int curX = ps.x;
+            int curY = ps.y;
+
             for (int i = 0; i < 4; i++) {
-                int tempX = x + dx[i];
-                int tempY = y + dy[i];
-                if (tempX >= 0 && tempY >= 0 && tempX < w + 2 && tempY < h + 2 && !visited[tempY][tempX]) {
-                    if (map[tempY][tempX] != '*') {
-                        if (map[tempY][tempX] == '#') {
-                            pq.add(new Position(tempX, tempY, count + 1));
-                        } else
-                            pq.add(new Position(tempX, tempY, count));
-                        visited[tempY][tempX] = true;
-                    }
+                int nxtX = curX + dx[i];
+                int nxtY = curY + dy[i];
+
+                if (nxtX < 0 || nxtX >= h + 2 || nxtY < 0 || nxtY >= w + 2) continue;
+                if (visited[nxtX][nxtY]) continue;
+                if (map[nxtX][nxtY] == '*') continue;
+
+                if (map[nxtX][nxtY] == '#') {
+                    pq.add(new Person(nxtX, nxtY, ps.score + 1));
+                    result[nxtX][nxtY] = ps.score + 1;
+                } else {
+                    pq.add(new Person(nxtX, nxtY, ps.score));
+                    result[nxtX][nxtY] = ps.score;
                 }
+                visited[nxtX][nxtY] = true;
             }
         }
         return result;
     }
 
-    static int minDoor(int h, int w) {
-        int result = Integer.MAX_VALUE;
-        for (int i = 1; i < h + 1; i++) {
-            for (int j = 1; j < w + 1; j++) {
-                if (map[i][j] == '*') continue;
-                int sum = prisionOne[i][j] + prisionTwo[i][j] + prisonThree[i][j];
-                if (map[i][j] == '#') sum -= 2;
-
-                if (result > sum && visited[i][j]) {
-                    result = sum;
-                }
+    private static void calculate(int [][] one, int[][] two, int[][] three) {
+        for(int i=0; i<h+2; i++) {
+            for(int j=0; j<w+2; j++) {
+                int tmp = one[i][j] + two[i][j] + three[i][j];
+                if(map[i][j] == '*') continue;
+                if(map[i][j] == '#') tmp-=2;
+                if(visited[i][j])ans = Math.min(ans, tmp);
             }
         }
-        return result;
+
     }
 }
