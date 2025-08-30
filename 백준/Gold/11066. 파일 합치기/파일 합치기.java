5396bbabf8f55271b842cee08ad2576a25605510
Minu Kim <kmw106933@naver.com>
2025-08-31 02:13:01 +0900

    [Gold III] Title: 파일 합치기, Time: 604 ms, Memory: 32944 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.java
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.java
@@ -1,47 +1,45 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] files;
-    static int[][] dp;
-    static int[] sum;
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in), 1<<16);
+    static final int INF = Integer.MAX_VALUE/3-1;
 
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int T = Integer.parseInt(br.readLine());
-        for(int i=0; i<T; i++) {
-            int K = Integer.parseInt(br.readLine());
-            files = new int[K+1];
-            dp = new int[K+1][K+1];
-            sum = new int[K+1];
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int k=1; k<=K; k++) {
-                files[k] = Integer.parseInt(st.nextToken());
-                Arrays.fill(dp[k], Integer.MAX_VALUE);
-                sum[k] = sum[k-1] + files[k];
-            }
-            sb.append(mergefiles(1, K)).append('\n');
-        }
-        System.out.print(sb.toString());
+        new Main().solution();
     }
-    private static int mergefiles(int start, int end) {
-        if (dp[start][end] == Integer.MAX_VALUE) {
-            if(start == end) {
-                dp[start][end] = 0;
-            } else if (start+1 == end) {
-                dp[start][end] = files[start] + files[end];
-            } else {
-                int subSum = sum[end] - sum[start-1];
-                dp[start][end] = mergefiles(start+1, end) + subSum;
-                for(int i=start+1; i<end; i++) {
-                    dp[start][end] = Math.min(dp[start][end], mergefiles(start, i) +  mergefiles(i + 1, end) + subSum);
+
+    private void solution() throws Exception {
+        int t = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while (t-->0) sb.append(solve()).append('\n');
+        System.out.print(sb);
+    }
+
+    private int solve() throws Exception {
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int[] prefixSum = new int[n];
+        int[][] dp = new int[n][n];
+        for (int[] row : dp) Arrays.fill(row, INF);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            dp[i][i] = 0;
+        }
+        prefixSum[0] = arr[0];
+        for (int i = 1; i < n; i++) prefixSum[i] = arr[i] + prefixSum[i - 1];
+
+        for (int gap = 1; gap < n; gap++) {
+            for (int s = 0; s < n - gap; s++) {
+                for (int i = s; i < s+gap; i++) {
+                    dp[s][s+gap] = Math.min(dp[s][s+gap], dp[s][i] + dp[i+1][s+gap] + prefixSum[s+gap] - (s==0?0:prefixSum[s-1]));
                 }
             }
         }
-        return dp[start][end];
+        return dp[0][n - 1];
     }
 }
