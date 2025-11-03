beb9098673d79845faa34ddedf11495e3c0ffb95
Minu Kim <kmw106933@naver.com>
2025-11-03 21:19:49 +0900

    [Gold V] Title: 감시 피하기, Time: 140 ms, Memory: 16292 KB -BaekjoonHub

--- 백준/Gold/18428. 감시 피하기/감시 피하기.java
+++ 백준/Gold/18428. 감시 피하기/감시 피하기.java
@@ -6,72 +6,89 @@ import java.util.StringTokenizer;
 public class Main {
 
     static int N;
-    static char map[][];
-    static int dx[] = {-1,1,0,0};
-    static int dy[] = {0,0,-1,1};
-    static class Node {
-        int x,y;
+    static char[][] map;
+    static ArrayList<Node> teachers;
+    static ArrayList<Node> students;
+    static ArrayList<Node> empty;
+    static boolean[] visited;
+
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
+
+    public static class Node {
+        int x, y;
         public Node(int x, int y) {
             this.x = x;
             this.y = y;
         }
     }
-    static ArrayList<Node> teachers = new ArrayList<>();
-    static ArrayList<Node> blanks = new ArrayList<>();
-    static boolean[] visited;
-    public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         N = Integer.parseInt(br.readLine());
-        map = new char[N][N];
+        teachers = new ArrayList<>();
+        students = new ArrayList<>();
+        empty = new ArrayList<>();
+
+        map = new char[N+1][N+1];
 
         for(int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
             for(int j=0; j<N; j++) {
                 map[i][j] = st.nextToken().charAt(0);
-                if(map[i][j] == 'T') teachers.add(new Node(i,j));
-                if(map[i][j] == 'X') blanks.add(new Node(i,j));
             }
         }
-
-        visited = new boolean[blanks.size()];
-        comb(0,0);
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<N; j++) {
+                if(map[i][j] == 'S') {
+                    students.add(new Node(i, j));
+                }
+                else if(map[i][j] == 'T') {
+                    teachers.add(new Node(i, j));
+                }
+                else empty.add(new Node(i, j));
+            }
+        }
+        visited = new boolean[empty.size()];
+        track(0,0);
         System.out.println("NO");
     }
 
-    public static void comb(int cnt, int start) {
+    public static void track(int cnt, int start) {
         if(cnt == 3) {
             if(dfs()) {
                 System.out.println("YES");
                 System.exit(0);
             }
-
             return;
         }
-
-        for(int i= start; i<blanks.size(); i++) {
+        for(int i=start; i< empty.size(); i++) {
             if(visited[i]) continue;
             visited[i] = true;
-            Node n = blanks.get(i);
+            Node n = empty.get(i);
             map[n.x][n.y] = 'O';
-            comb(cnt+1, i+1);
-            map[n.x][n.y] = 'X';
+            track(cnt+1, i+1);
             visited[i] = false;
+            map[n.x][n.y] = 'X';
         }
     }
 
-    private static boolean dfs() {
-        for(Node n : teachers) {
-            int x = n.x;
-            int y = n.y;
+    public static boolean dfs() {
+        for(Node t : teachers) {
+            int x = t.x;
+            int y = t.y;
 
             for(int i=0; i<4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
 
-                while (isIn(nx,ny)) {
-                    if(map[nx][ny] == 'S') return false;
+                while (isIn(nx, ny)) {
+
+                    if(map[nx][ny] == 'S') {
+                        return false;
+                    }
                     if(map[nx][ny] == 'O') break;
 
                     nx += dx[i];
@@ -81,9 +98,7 @@ public class Main {
         }
         return true;
     }
-
-    private static boolean isIn(int nx, int ny) {
-        if(nx>=0&ny>=0&&nx<N && ny<N) return true;
-        return false;
+    public static boolean isIn(int x, int y) {
+        return x >= 0 && x < N && y >=0 && y < N;
     }
 }
