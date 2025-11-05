154e11403864e15b67719254f95ae8a2635fd8cf
Minu Kim <kmw106933@naver.com>
2025-11-05 16:22:49 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 82.73 ms, Memory: 95.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,6 +1,6 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 05일 16:22:15
+2025년 11월 05일 16:22:47
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -4,20 +4,15 @@
 
 using namespace std;
 
-map<string, int> m;
+unordered_map<string, int> m;
 
 string solution(vector<string> participant, vector<string> completion) {
-    string s = "";
-    for(int i=0; i<participant.size(); i++) {
-        m[participant[i]]++;
-    }
+    for(auto p : participant) m[p]++;
+    for(auto c : completion) m[c]--;
     
-    for(int i=0; i<completion.size(); i++){
-        m[completion[i]]--;
+    string answer = "";
+    for(auto iter=m.begin(); iter!=m.end(); iter++) {
+        if (iter->second > 0) answer += iter->first;
     }
-    
-    for(auto iter = m.begin(); iter != m.end(); iter++) {
-        if(iter->second > 0) s += iter->first;
-    }
-    return s;
+    return answer;
 }
