6e8380b5826eb3bd331374c07b029225b94f57c1
Minu Kim <kmw106933@naver.com>
2025-11-05 15:20:06 +0900

    [level 1] Title: 예산, Time: 0.49 ms, Memory: 92.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int total = 0;
+        int answer = 0;
+        Arrays.sort(d);
+        
+        for(int i=0; i<d.length; i++) {
+            if(total + d[i] <= budget) {
+                total += d[i];
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
