88e63e1fa0b684743d283d07d3daff54fd19330e
Minu Kim <kmw106933@naver.com>
2025-11-12 12:30:08 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 10.70 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String today, String[] terms, String[] privacies) {
+        List<Integer> answer = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
+        
+        int checkDate = getDate(today);
+        
+        for(String s : terms) {
+            String[] term = s.split(" ");
+            map.put(term[0], Integer.parseInt(term[1]));
+        }
+        
+        for(int i=0; i<privacies.length; i++) {
+            String[] privacy = privacies[i].split(" ");
+            
+            if(getDate(privacy[0]) + (map.get(privacy[1]) * 28) <= checkDate) {
+                answer.add(i+1);
+            }
+        }
+        
+        return answer.stream().mapToInt(i->i).toArray();
+ 
+    }
+    
+    public static int getDate(String date) {
+        String[] arr = date.split("\\.");
+        
+        int year = Integer.parseInt(arr[0]);
+        int month = Integer.parseInt(arr[1]);
+        int day = Integer.parseInt(arr[2]);
+        
+        return (year * 12 * 28) + month*28 + day;
+    }
+}
