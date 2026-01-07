f2b9fc95613ffa56929f8470e85f0b12b89fdc2e
김민우 <kmw106933@naver.com>
2026-01-08 01:39:13 +0900

    [Gold V] Title: 색종이와 가위, Time: 100 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Gold/20444. 색종이와 가위/색종이와 가위.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    static int n;
+    static long k;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Long.parseLong(st.nextToken());
+
+        long left = 0;
+        long right = n/2;
+        while (left <= right) {
+            long row = (left + right) / 2;
+            long col = (n - row);
+
+            if(check(row, col) == k) {
+                System.out.println("YES");
+                return;
+            }
+            else if(check(row, col) > k) right = row - 1;
+            else if(check(row, col) < k) left = row + 1;
+        }
+        System.out.println("NO");
+    }
+    private static long check(long row, long col) {
+        return (row + 1) * (col + 1);
+    }
+}
