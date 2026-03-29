51d492c671f8b205756c9f682245d186d4ad7759
김민우 <kmw106933@naver.com>
2026-03-30 06:01:25 +0900

    [level 2] Title: 서버 증설 횟수, Time: 1.64 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -1,36 +1,30 @@
 import java.util.*;
+import java.io.*;
 
 class Solution {
     public int solution(int[] players, int m, int k) {
-        ArrayList<Server> servers = new ArrayList<>();
-        ArrayList<Integer> test = new ArrayList<>();
+        List<ServerType> servers = new ArrayList<>();
+        int ans = 0;
         
-        int answer = 0;
-
         for(int i=0; i<players.length; i++) {
+            
             for(int j=servers.size()-1; j>=0; j--) {
-                if(servers.get(j).time <= i) {
-                    servers.remove(j);
-                }
+                if(servers.get(j).endtime <= i) servers.remove(j);
             }
- 
+            
             while(players[i]/m > servers.size()) {
-                servers.add(new Server(i+k));
-                answer++;
-            } 
-            test.add(servers.size());
+                servers.add(new ServerType(i+k));
+                ans++;
+            }
         }
-        for(int t: test) {
-            System.out.print(t);
-            System.out.print(" ");
-        }
-        
-        return answer;
+        return ans;
     }
-    class Server {
-        int time;
-        Server(int time) {
-            this.time = time;
+    
+    class ServerType {
+        int endtime;
+        
+        ServerType(int endtime) {
+            this.endtime = endtime;
         }
     }
 }
