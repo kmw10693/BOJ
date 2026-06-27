a9dc62eaad9e7ee90845684635bcbd87545686e4
김민우 <kmw106933@naver.com>
2026-06-28 01:04:05 +0900

    [level 3] Title: 상담원 인원, Time: 12.13 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/3/214288. 상담원 인원/상담원 인원.java
+++ 프로그래머스/3/214288. 상담원 인원/상담원 인원.java
@@ -3,39 +3,36 @@ import java.io.*;
 
 class Solution {
     public int solution(int k, int n, int[][] reqs) {
-        int total = n - k;
-        
-        // 멘토 수
+        int total = n-k;
         int[] mentos = new int[k+1];
         
         Arrays.fill(mentos, 1);
         
         while(total > 0) {
-            
-            int realmentoindex = -1;
+            int realmentoidx = -1;
             int eachmintime = Integer.MAX_VALUE;
+            
             for(int i=1; i<=k; i++) {
                 mentos[i]++;
+                
                 if(eachmintime > gettotalwaittime(reqs, mentos, k)) {
-                    realmentoindex = i;
+                    realmentoidx = i;
                     eachmintime = gettotalwaittime(reqs, mentos, k);
                 }
                 mentos[i]--;
             }
-            mentos[realmentoindex]++;
+            mentos[realmentoidx]++;
             total--;
         }
         
-      
-     
         int ans = 0;
         ans = gettotalwaittime(reqs, mentos, k);
         return ans;
     }
     
     private int gettotalwaittime(int[][] reqs, int[] mentos, int k) {
-        
         int temptime = 0;
+        
         for(int i=1; i<=k; i++) {
             temptime += checkmintime(reqs, mentos, i);
         }
@@ -46,7 +43,7 @@ class Solution {
         int eachmintime = 0;
         Queue<Integer> pq = new PriorityQueue<>();
         
-        for(int [] req : reqs) {
+        for(int[] req : reqs) {
             int startmin = req[0];
             int duration = req[1];
             int targetdiv = req[2];
