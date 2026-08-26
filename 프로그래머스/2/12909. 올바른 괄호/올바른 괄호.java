0f18630f480630dde4960dd3eb140357a1516252
김민우 <kmw106933@naver.com>
2026-08-26 22:42:05 +0900

    [level 2] Title: 올바른 괄호, Time: 11.18 ms, Memory: 57.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for(int i=0; i<s.length(); i++) {
+            if(s.charAt(i) == '(') {
+                stack.add(s.charAt(i));
+            }
+            if(s.charAt(i) == ')') {
+                if(stack.isEmpty()) {
+                    return false;
+                }
+                else {
+                    if(stack.peek() == '(') {
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+        if(!stack.isEmpty()) return false;
+        return true;
+    }
+}
