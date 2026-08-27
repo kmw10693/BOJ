8b63cb0a204ee60a1d3846bff1cfd901331983e4
김민우 <kmw106933@naver.com>
2026-08-27 16:12:16 +0900

    [level 2] Title: 피로도, Time: 2.90 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
 
 ### 성능 요약
 
-메모리: 88.1 MB, 시간: 3.05 ms
+메모리: 79.3 MB, 시간: 2.90 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 29일 21:03:06
+2026년 08월 27일 16:12:13
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -2,28 +2,23 @@ import java.util.*;
 import java.io.*;
 
 class Solution {
-    int result = Integer.MIN_VALUE;
+    int ans = Integer.MIN_VALUE;
     public int solution(int k, int[][] dungeons) {
         boolean[] visited = new boolean[dungeons.length];
-        for(int i=0; i<dungeons.length; i++) {
-            if(k >= dungeons[i][0] && visited[i] == false) {
-                visited[i] = true;
-                dfs(k-dungeons[i][1], dungeons, 1, visited);
-                visited[i] = false;
-            }
-        }
-        return result;
+        dfs(k, dungeons, visited, 0);
+        return ans;
     }
     
-    public void dfs(int k, int[][] dungeons, int cnt, boolean[] visited) {
+    public void dfs(int k, int[][] dungeons, boolean[] visited, int cnt) {
+        ans = Math.max(ans, cnt);
+        
         for(int i=0; i<dungeons.length; i++) {
             if(visited[i]) continue;
-            if(k >= dungeons[i][0] && visited[i] == false) {
-                visited[i] = true;
-                dfs(k-dungeons[i][1], dungeons, cnt+1, visited);
-                visited[i] = false;
-            }
+            if(k < dungeons[i][0]) continue;
+            
+            visited[i] = true;
+            dfs(k-dungeons[i][1], dungeons, visited, cnt+1);
+            visited[i] = false;
         }
-        result = Math.max(result, cnt);
     }
 }
