03286c26ff50dc9d1fc8bfea60b9352601b516bb
김민우 <kmw106933@naver.com>
2026-08-28 17:03:50 +0900

    [level 3] Title: 가장 먼 노드, Time: 23.95 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/README.md
+++ 프로그래머스/3/49189. 가장 먼 노드/README.md
@@ -1,10 +1,10 @@
 # [level 3] 가장 먼 노드 - 49189 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49189?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49189?gad_source=1&gad_campaignid=22356298761&gbraid=0AAAAAC_c4nACC4X997OofEY5m-fy_EnBL&gclid=CjwKCAjw48TUBhBREiwAK0GnQazzaD8oNxnM8xNPN47-OlIw6_WR4FQUG8-reYkLyriV1aizlUJzlRoCQsMQAvD_BwE) 
 
 ### 성능 요약
 
-메모리: 112 MB, 시간: 14.61 ms
+메모리: 102 MB, 시간: 23.95 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 08월 27일 23:15:14
+2026년 08월 28일 17:03:48
 
 ### 문제 설명
 
--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,6 +1,48 @@
+import java.util.*;
+import java.io.*;
+
 class Solution {
     public int solution(int n, int[][] edge) {
-        int answer = 0;
-        return answer;
+        List<Integer>[] graph = new ArrayList[n+1];
+        
+        for(int i=1; i<=n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        
+        for(int[] e : edge) {
+            int from = e[0];
+            int to = e[1];
+            
+            graph[from].add(to);
+            graph[to].add(from);
+        }
+        
+        int[] distance = new int[n+1];
+        Arrays.fill(distance, -1);
+        
+        Queue<Integer> q = new LinkedList<>();
+        distance[1] = 0;
+        q.add(1);
+        
+        while(!q.isEmpty()) {
+            int cur = q.poll();
+            
+            for(int nxt : graph[cur]) {
+                if(distance[nxt] != -1) continue;
+                distance[nxt] = distance[cur] + 1;
+                q.add(nxt);
+            }
+        }
+        int maxdis = Integer.MIN_VALUE;
+        
+        for(int i=1; i<=n; i++) {
+            maxdis = Math.max(maxdis, distance[i]);
+        }
+        
+        int ans = 0;
+        for(int i=1; i<=n; i++) {
+            if(maxdis == distance[i]) ans++;
+        }
+        return ans;
     }
 }
