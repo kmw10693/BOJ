0fd89998d2e54a0d08c4afd308ccdcf428f7a52d
김민우 <kmw106933@naver.com>
2026-09-04 01:47:54 +0900

    [level 2] Title: 광물 캐기, Time: 1.16 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -3,40 +3,48 @@ import java.io.*;
 
 class Solution {
     public int solution(int[] picks, String[] minerals) {
-        int answer = 0;
-        int[][] tired = {{1,1,1}, {5,1,1}, {25, 5, 1}};
+        int pickcount = picks[0] + picks[1] + picks[2];
         
-        int totalpick = picks[0] + picks[1] + picks[2];
-        int maxlength = Math.min(minerals.length, totalpick * 5);
+        int maxcount = Math.min(pickcount * 5, minerals.length);
+        List<int[]> arr = new ArrayList<>();
         
-        List<int[]> groups = new ArrayList<>();
-        
-        for(int i=0; i<minerals.length; i+=5) {
+        for(int i=0; i<maxcount; i+=5) {
+            int diamond = 0;
+            int iron = 0;
+            int stone = 0;
             
-            int groupdia = 0;
-            int groupiron = 0;
-            int groupstone = 0;
-            for(int j=i; j<i+5 && j<maxlength; j++) {
-                if(minerals[j].equals("diamond")) groupdia++;
-                else if(minerals[j].equals("iron")) groupiron++;
-                else groupstone++;
+            for(int j=i; j<Math.min(maxcount, i+5); j++) {
+                if(minerals[j].equals("diamond")) {
+                    diamond++;
+                } else if(minerals[j].equals("iron")) {
+                    iron++;
+                } else {
+                    stone++;
+                }
             }
-            groups.add(new int[]{groupdia, groupiron, groupstone});
+            
+            arr.add(new int[]{diamond, iron, stone});
         }
         
-        groups.sort((a,b) -> {
-            int psum1 = a[0]*25 + a[1]*5 + a[2];
-            int psum2 = b[0]*25 + b[1]*5 + b[2];
-            return psum2 - psum1;
+        arr.sort((a,b) -> {
+            if(a[0] != b[0]) {
+                return b[0] - a[0];
+            }
+            return b[1] - a[1];
         });
         
-        for(int[] g : groups) {
-            for(int i=0; i<picks.length; i++) {
-                if(picks[i] > 0) {
-                    answer += g[0] * tired[i][0] + g[1] * tired[i][1] + g[2] * tired[i][2];
-                    picks[i]--;
-                    break;
-                }
+        int answer = 0;
+        
+        for(int[] a : arr) {
+            if(picks[0] > 0) {
+                answer += (a[0] + a[1] + a[2]); 
+                picks[0]--;
+            } else if(picks[1] > 0) {
+                answer += (a[0] *5 + a[1] + a[2]);
+                picks[1]--;
+            } else {
+                answer += (a[0]*25 + a[1]*5 + a[2]);
+                picks[2]--;
             }
         }
         return answer;
