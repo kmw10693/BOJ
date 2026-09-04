e35e6d394165743c176ca6f532398f5e66f134e8
김민우 <kmw106933@naver.com>
2026-09-04 14:33:42 +0900

    [level 2] Title: 거리두기 확인하기, Time: 3.06 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -2,62 +2,59 @@ import java.util.*;
 import java.io.*;
 
 class Solution {
-    boolean arrange = true;
+    boolean result = true;
+    
     int[] dx = {1,-1,0,0};
     int[] dy = {0,0,1,-1};
     
-    int n, m;
-    
-    public int[] solution(String[][] places) {
+    public void dfs(String[] place, int cnt, int curx, int cury, boolean[][] isvisited) {
+        if(result == false) return; 
         
-        List<Integer> ans = new ArrayList<>();
-        
-        for(int i=0; i<places.length; i++) {
-            for(int j=0; j<places[i].length; j++) {
-                
-                for(int k=0; k<places[i][j].length(); k++) {
-                    if(places[i][j].charAt(k) == 'P' && arrange) {
-                        n = places[i].length;
-                        m = places[i][j].length();
-                        
-                        boolean[][] visited = new boolean[n][m];
-                        visited[j][k] = true;
-                        
-                        dfs(0, places[i], j, k, visited);
-                    }
-                }
-                
-            }
-            if(arrange) ans.add(1);
-            else ans.add(0);
-            
-            arrange = true;
-        }
-        return ans.stream().mapToInt(Integer::intValue).toArray();
-    }
-    
-    public void dfs(int cnt, String[] places, int curx, int cury, boolean[][] visited) {
-        if(cnt > 0 && cnt <= 2) {
-            if(places[curx].charAt(cury) == 'P') {
-                arrange = false;
-                return;
+        if(cnt <= 2 && cnt > 0) {
+            if(place[curx].charAt(cury) == 'P') {
+                result = false;
             }
         }
         
-        if(cnt == 2) {
-            return;
-        }
+        if(cnt == 2) return;
         
         for(int dir=0; dir<4; dir++) {
             int nxtx = curx + dx[dir];
             int nxty = cury + dy[dir];
             
-            if(nxtx < 0 || nxtx >= n || nxty < 0 || nxty >= m) continue;
-            if(visited[nxtx][nxty] || places[nxtx].charAt(nxty) == 'X') continue;
+            if(nxtx < 0 || nxtx >= place.length || nxty < 0 || nxty >= place[0].length()) continue;
+            if(place[nxtx].charAt(nxty) == 'X') continue;
+            if(isvisited[nxtx][nxty]) continue;
             
-            visited[nxtx][nxty] = true;
-            dfs(cnt+1, places, nxtx, nxty, visited);
-            visited[nxtx][nxty] = false;
+            isvisited[nxtx][nxty] = true;
+            dfs(place, cnt+1, nxtx, nxty, isvisited);
+            isvisited[nxtx][nxty] = false;
         }
+    }
+    
+    public int[] solution(String[][] places) {
+        
+        List<Integer> ans = new ArrayList<>();
+        
+        for(String[] place : places) {
+            int n = place.length;
+            int m = place[0].length();
+            
+            for(int i=0; i<n; i++) {
+                for(int j=0; j<m; j++) {
+                    if(place[i].charAt(j) == 'P') {
+                        boolean[][] isvisited = new boolean[n][m];
+                        isvisited[i][j] = true;
+                        dfs(place, 0, i, j, isvisited);
+                    } 
+                }
+            }
+            
+            if(result) ans.add(1);
+            else ans.add(0);
+            result = true;
+        }
+        
+        return ans.stream().mapToInt(Integer::intValue).toArray();
     }
 }
