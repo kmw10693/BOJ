5e99ba9893cf494e902113b4d02be0304160e54f
김민우 <kmw106933@naver.com>
2026-09-04 23:17:51 +0900

    [level 2] Title: 모음 사전, Time: 8.59 ms, Memory: 87.4 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,46 +1,27 @@
-import java.util.*;
-import java.io.*;
-
 class Solution {
     int ans = 0;
-    boolean isanswer = false;
+    boolean check = false;
+    
+    public void dfs(String s, String word) {
+        if(check) return;
+        if(s.length() > 0 && s.length() <= 5) {
+            ans++;
+            if(s.equals(word)) {
+                check = true;
+                return;
+            }
+        }
+        if(s.length() == 5) return;
+        
+        dfs(s+"A", word);
+        dfs(s+"E", word);
+        dfs(s+"I", word);
+        dfs(s+"O", word);
+        dfs(s+"U", word);
+    }
     
     public int solution(String word) {
         dfs("", word);
-        return ans-1;
-    }
-    
-    public void dfs(String start, String word) {
-        if(isanswer) return;
-        
-        ans++;
-        if(start.equals(word)) {
-            isanswer = true;
-            return;
-        }
-        
-        if(start.length() == 5) {
-            return;
-        }
-        
-        start += "A";
-        dfs(start, word);
-        start = start.substring(0, start.length()-1);
-        
-        start += "E";
-        dfs(start, word);
-        start = start.substring(0, start.length()-1);
-        
-        start += "I";
-        dfs(start, word);
-        start = start.substring(0, start.length()-1);
-        
-        start += "O";
-        dfs(start, word);
-        start = start.substring(0, start.length()-1);
-        
-        start += "U";
-        dfs(start, word);
-        start = start.substring(0, start.length()-1);
+        return ans;
     }
 }
