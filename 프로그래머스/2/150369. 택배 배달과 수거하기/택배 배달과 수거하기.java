b6def7c38a1c3aaf20c9256bcff734de757a227a
김민우 <kmw106933@naver.com>
2026-09-05 00:18:34 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 15.29 ms, Memory: 93.7 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -1,17 +1,61 @@
+import java.util.*;
+import java.io.*;
+
 class Solution {
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
         long answer = 0;
-        int deli = 0, pick = 0;
-        for(int i=n-1; i>=0; i--) {
-            deli -= deliveries[i];
-            pick -= pickups[i];
+        int have = 0;
+        int curpos = -1;
+        
+        // 추가
+        int deliveryIdx = n - 1;
+        int pickupIdx = n - 1;
+        
+        while(true) {
             
-            while(deli < 0 || pick < 0) {
-                deli += cap;
-                pick += cap;
-                answer += (i+1)*2;
+             // 이미 끝난 위치 앞으로 이동
+            while (deliveryIdx >= 0 && deliveries[deliveryIdx] == 0) {
+                deliveryIdx--;
             }
+
+            while (pickupIdx >= 0 && pickups[pickupIdx] == 0) {
+                pickupIdx--;
+            }
+            
+            if (deliveryIdx < 0 && pickupIdx < 0) {
+                break;
+            }
+            
+            
+            for(int i=deliveryIdx; i>=0; i--) {
+                while(have < cap && deliveries[i] > 0) {
+                    if(curpos < i) {
+                        answer += (i - curpos)*2;
+                        curpos = i;
+                    }
+                    deliveries[i]--;
+                    have++;
+                }
+                if (have == cap) break;
+            }
+            have = 0;
+            
+            for(int i=pickupIdx; i>=0; i--) {
+                while(have < cap && pickups[i] > 0) {
+                    if(curpos < i) {
+                        answer += (i - curpos)*2;
+                        curpos = i;
+                    }
+                    pickups[i]--;
+                    have++;
+                }
+                if (have == cap) break;
+
+            }
+            have = 0;
+            curpos = -1; 
         }
+        
         return answer;
     }
 }
