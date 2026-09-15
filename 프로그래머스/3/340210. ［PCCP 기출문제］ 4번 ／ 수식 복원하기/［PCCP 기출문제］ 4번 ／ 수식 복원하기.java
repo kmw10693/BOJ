8d31ea9fd4a680da30684fca6ab8e77baba30dc0
김민우 <kmw106933@naver.com>
2026-09-15 13:13:21 +0900

    [level 3] Title: [PCCP 기출문제] 4번 / 수식 복원하기, Time: 5.88 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/3/340210. ［PCCP 기출문제］ 4번 ／ 수식 복원하기/［PCCP 기출문제］ 4번 ／ 수식 복원하기.java
+++ 프로그래머스/3/340210. ［PCCP 기출문제］ 4번 ／ 수식 복원하기/［PCCP 기출문제］ 4번 ／ 수식 복원하기.java
@@ -1,145 +1,131 @@
 import java.util.*;
-import java.io.*;
 
 class Solution {
-    public String[] solution(String[] expressions) {
-        int[] jinbups = {2,3,4,5,6,7,8,9};
-        boolean[] correctjinbups = new boolean[8];
+    
+    private int toDecimal(String number, int base) {
+        int result = 0;
         
-        for(int i=0; i<8; i++) {
-            correctjinbups[i] = true;
-        }
-            
-        List<Expression> xlist = new ArrayList();
-        
-        for(String expression : expressions) {
-            String[] splitlist = expression.split(" ");
-            String a = splitlist[0];
-            int op = (splitlist[1].equals("+")) ? 1 : -1;
-            String b = splitlist[2];
-            
-            String result = splitlist[4];
-            
-            // a, b, result 돌면서 이하인거 모두 false로 만들기
-            for(int i=0; i<a.length(); i++) {
-                int eachnumber = a.charAt(i) - '0';
-                
-                for(int j=eachnumber-2; j>=0; j--) {
-                    correctjinbups[j] = false;
-                }
-            }
-            
-            for(int i=0; i<b.length(); i++) {
-                int eachnumber = b.charAt(i) - '0';
-                
-                for(int j=eachnumber-2; j>=0; j--) {
-                    correctjinbups[j] = false;
-                }
-            }
-            
-            if(!result.equals("X")) {
-                for(int i=0; i<result.length(); i++) {
-                    int eachnumber = result.charAt(i) - '0';
-
-                    for(int j=eachnumber-2; j>=0; j--) {
-                        correctjinbups[j] = false;
-                    }
-                }
-            }
-            
-            // X가 아니면, 각 수식마다 진법 계산을 모두 적용해보고, 지우기
-            if(!result.equals("X")) {
-                for(int i=0; i<8; i++) {
-                    if(!correctjinbups[i]) continue;
-                    int converta = Integer.parseInt(splitlist[0], jinbups[i]);
-                    int convertb = Integer.parseInt(splitlist[2], jinbups[i]);
-                    
-                    int convertresult = Integer.parseInt(splitlist[4], jinbups[i]);
-                    
-                    if(converta + op*convertb != convertresult) correctjinbups[i] = false;
-                }
-            } 
-            // X이면 X 리스트에 저장 하기
-            else {
-                xlist.add(new Expression(a, b, op));
-            }
-        }
-        
-        List<String> tmpresult = new ArrayList();
-        
-        for(int i=0; i<xlist.size(); i++) {
-            Expression eachexp = xlist.get(i);
-            
-            int lastresult = -1;
-            boolean alreadyinput = false;
-            for(int j=0; j<8; j++) {
-                if(!correctjinbups[j]) continue;
-                
-                String strcurresult = Integer.toString(Integer.parseInt(eachexp.a, jinbups[j]) + eachexp.op * Integer.parseInt(eachexp.b, jinbups[j]), jinbups[j]);
-                int curresult = Integer.parseInt(strcurresult);
-                
-                if(lastresult != -1) {
-                    if(lastresult != curresult) {
-                        StringBuilder sb = new StringBuilder();
-                        sb.append(eachexp.a);
-                        sb.append(" ");
-                        if(eachexp.op == -1) sb.append("-");
-                        else sb.append("+");
-                        sb.append(" ");
-                        sb.append(eachexp.b);
-                        sb.append(" = ");
-                        sb.append("?");
-                        tmpresult.add(sb.toString());
-                        alreadyinput = true;
-                        break;
-                    }
-                } 
-                lastresult = curresult;
-            }
-            
-            if(alreadyinput) continue;
-            
-            if(lastresult != -1) {
-                StringBuilder sb = new StringBuilder();
-                sb.append(eachexp.a);
-                sb.append(" ");
-                if(eachexp.op == -1) sb.append("-");
-                else sb.append("+");
-                sb.append(" ");
-                sb.append(eachexp.b);
-                sb.append(" = ");
-                sb.append(lastresult);
-                tmpresult.add(sb.toString());
-            }
-            else {
-                StringBuilder sb = new StringBuilder();
-                sb.append(eachexp.a);
-                sb.append(" ");
-                if(eachexp.op == -1) sb.append("-");
-                else sb.append("+");
-                sb.append(" ");
-                sb.append(eachexp.b);
-                sb.append(" = ");
-                sb.append("?");
-                tmpresult.add(sb.toString());
-            }
-        }
-        
-        String[] result = new String[tmpresult.size()];
-        for(int i=0; i<tmpresult.size(); i++) {
-            result[i] = tmpresult.get(i);
+        for(char c : number.toCharArray()) {
+            int digit = c - '0';
+            result = result * base + digit;
         }
         return result;
     }
     
-    public class Expression {
-        String a, b;
-        int op;
+    private String fromDecimal(int number, int base) {
+        if(number == 0) return "0";
         
-        Expression(String a, String b, int op) {
-            this.a = a;
-            this.b = b;
-            this.op = op;
+        StringBuilder sb = new StringBuilder();
+
+        while(number > 0) {
+            int digit = number % base;
+            sb.append(digit);
+            number /= base;
         }
+        return sb.reverse().toString();
+    }
+    
+    private boolean valid(String number, int base) {
+        for(char c : number.toCharArray()) {
+            
+            int digit = c - '0';
+            
+            if(digit >= base) return false;
+        }
+        return true;
+    }
+    
+    public String[] solution(String[] expressions) {
+        List<Integer> bases = new ArrayList<>();
+        
+        for(int base = 2; base <= 9; base++) {
+            
+            boolean possible = true;
+            
+            for(String expression : expressions) {
+                String[] split = expression.split(" ");
+                String A = split[0];
+                String op = split[1];
+                String B = split[2];
+                String C = split[4];
+                
+                if(!valid(A, base) || !valid(B, base)) {
+                    possible = false;
+                    break;
+                }
+                
+                if(!C.equals("X")) {
+                    if(!valid(C, base)) {
+                        possible = false;
+                        break;
+                    }
+                    
+                    int a = toDecimal(A, base);
+                    int b = toDecimal(B, base);
+                    int c = toDecimal(C, base);
+                    
+                    int result;
+                    
+                    if(op.equals("+")) {
+                        result = a + b;
+                    } else {
+                        result = a - b;
+                    }
+                    
+                    if(result != c) {
+                        possible = false;
+                        break;
+                    }
+                }
+            }
+            
+            if(possible) {
+                bases.add(base);
+            }
+        }
+        
+        List<String> answer = new ArrayList<>();
+        
+        for(String expression : expressions) {
+            String[] split = expression.split(" ");
+            
+            String A = split[0];
+            String op = split[1];
+            String B = split[2];
+            String C = split[4];
+            
+            if(!C.equals("X")) continue;
+            
+            String result = null;
+            boolean same = true;
+            
+            for(int base : bases) {
+                int a = toDecimal(A, base);
+                int b = toDecimal(B, base);
+                
+                int value;
+                
+                if(op.equals("+")) {
+                    value = a + b;
+                } else {
+                    value = a - b;
+                }
+                
+                String current = fromDecimal(value, base);
+                
+                if(result == null) {
+                    result = current;
+                }
+                else if(!result.equals(current)) {
+                    same = false;
+                    break;
+                }
+            }
+            if(!same) {
+                result = "?";
+            }
+            answer.add(A + " " + op + " " + B + " = " + result);
+        }
+        return answer.toArray(new String[0]);
     }
 }
